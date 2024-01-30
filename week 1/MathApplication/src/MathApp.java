600ff920718030c1a7abc74a391a99bd62e02687
Gabriel Omoregie <gabrielomor99@gmail.com>
2024-01-30 15:53:13 -0500

    completed exercise

--- week 1/MathApplication/src/MathApp.java
+++ week 1/MathApplication/src/MathApp.java
@@ -12,8 +12,28 @@ int smallestValue = Math.min(carPrice, truckPrice);
 
 float radius = 7.25f;
 double area_of_circle = Math.PI*Math.pow(radius,2);
+    System.out.println("The area of the circle is: " + Math.round(area_of_circle));
 
-        System.out.println("The area of the circle is: " + area_of_circle);
+    double number = 5.0;
+    double squareRoot = Math.sqrt(number);
+        System.out.println("The square root of the number is: " + squareRoot);
+
+        double positiveValue = -3.5;
+        System.out.println("The positive value is: " + Math.abs(positiveValue));
+
+        int x1 = 5;
+        int y1 = 10;
+        int x2 = 85;
+        int y2 = 50;
+        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("The distance between the points is: " + distance);
+
+
+        double randomNum = Math.random() * 10;
+        System.out.println("The random Num is: " + randomNum);
+
+
+
 
     }
 }
